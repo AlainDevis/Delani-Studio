a053d9dbf0ac9139b20fa6f6e809ab04191f025f
AlainDevis <devisalain04@gmail.com>
2020-10-19 13:39:57 +0200

    changes in jquerry

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,21 @@
 
 $(document).ready(function(){
+    $("div.design").click(function() {
+        $(".designshow").toggle();
+        $(".designhide").toggle();
+      });
+      $("div.development").click(function() {
+        $(".developmentshow").toggle();
+        $(".developmenthide").toggle();
+      });
+      $("div.product").click(function() {
+        $(".productshow").toggle();
+        $(".producthide").toggle();
+      });
 
-    $('#work4').hover(function(){
-    $(this).show("<p style='backgroung:#ccc;color:#fff'>Color Gradient</p>");
-    }, function(){
-    $(this).html("<img src='./Images/portfolio/work4.jpg'>");
-    });
-
+    $('div.porto').hover(function() {
+        $('div.porto').find('div.overlay').fadeIn();
+        }, function() {
+        $('div.porto').find('div.overlay').fadeOut();
+        });
 });
